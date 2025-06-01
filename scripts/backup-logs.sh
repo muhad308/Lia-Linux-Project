d0427024136de922cb264156d1bdc514158c2678
Muhammad Adnan <muhad.adnan86@gmail.com>
2025-06-01 11:17:05 +0000

    Deleted screenshots with outdated ping test

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 timestamp=$(date +%F_%H-%M-%S)
 backup_dir=~/lia-linux-project/logs/backups/$timestamp
 
@@ -7,3 +8,38 @@ mkdir -p "$backup_dir"
 cp -r /var/log/remote "$backup_dir"
 
 echo "Logs backed up to $backup_dir"
+
+# ⬇️ GitHub commit and push
+cd ~/lia-linux-project
+git add logs/backups/
+git commit -m "Daily log backup: $(date +%F_%H:%M:%S)"
+git push origin main
+#!/bin/bash
+
+timestamp=$(date +%F_%H-%M-%S)
+backup_dir=~/lia-linux-project/logs/backups/$timestamp
+
+mkdir -p "$backup_dir"
+cp -r /var/log/remote "$backup_dir"
+
+echo "Logs backed up to $backup_dir"
+
+# ⬇️ GitHub commit and push
+cd ~/lia-linux-project
+git add logs/backups/
+git commit -m "Daily log backup: $(date +%F_%H:%M:%S)"
+git push origin main
+
+
+
+
+
+
+
+#timestamp=$(date +%F_%H-%M-%S)
+#backup_dir=~/lia-linux-project/logs/backups/$timestamp
+
+#mkdir -p "$backup_dir"
+#cp -r /var/log/remote "$backup_dir"
+
+#echo "Logs backed up to $backup_dir"
